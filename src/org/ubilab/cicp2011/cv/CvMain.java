02ca17cd9ec0ed064466854d435c290b8935bb3c
Atsushi OHNO <atsushi-o@is.naist.jp>
2011-11-21 15:08:46 +0900

    パラメータが増えすぎたのでインスタンスの生成方式をBuilderパターンに変更

--- src/org/ubilab/cicp2011/cv/CvMain.java
+++ src/org/ubilab/cicp2011/cv/CvMain.java
@@ -1,12 +1,20 @@
 package org.ubilab.cicp2011.cv;
 
 import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import com.googlecode.javacv.*;
 import static com.googlecode.javacv.cpp.opencv_core.*;
 import static com.googlecode.javacv.cpp.opencv_highgui.*;
 
 /**
  * 盤面検出処理メインクラス
+ * <pre>
+ * インスタンスの生成にはBuilderクラスを使用する
+ * <blockquote>
+ * new CvMain.Builder(0).build();
+ * </blockquote>
+ * </pre>
  * @author atsushi-o
  * @since 2011/11/17
  */
@@ -17,29 +25,64 @@ public class CvMain {
     
     static {
         src = new CanvasFrame("Source");
+        src.setVisible(false);
         src.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        src.addWindowListener(new WindowAdapter() {
+			// ウィンドウが閉じるときに呼ばれる
+			@Override
+			public void windowClosing(WindowEvent e) {
+				AnalyticProcess.releaseMemStorage();
+			}
+		});
     }
     
     /**
-     * カメラのインデックスを指定してインスタンスを生成する
-     * @param camera OpenCVで使用するカメラのインデックス
-     * @since 2011/11/17
+     * CvMainのインスタンス生成クラス
+     * @since 2011/11/21
      */
-    public CvMain(int camera) {
-        this(camera, 1280, 960);
+    public static class Builder {
+        // Required param
+        private final int camera;
+        
+        // Optional param
+        private int width       = 1280;
+        private int height      = 960;
+        private boolean debug   = false;
+        
+        /**
+         * 必須パラメータを指定
+         * @param camera OpenCVで使用するカメラのインデックス
+         * @since 2011/11/21
+         */
+        public Builder(int camera) {
+            this.camera = camera;
+        }
+        
+        public Builder width(int val)       { width = val; return this; }
+        public Builder height(int val)      { height = val; return this; }
+        public Builder debug(boolean val)   { debug = val; return this; }
+        
+        /**
+         * CvMainのインスタンスを生成する
+         * @return CvMainのインスタンス
+         * @since 2011/11/21
+         */
+        public CvMain build() {
+            return new CvMain(this);
+        }
     }
+    
     /**
-     * カメラの入力解像度も指定した上でインスタンスを生成する
-     * @param camera OpenCVで使用するカメラのインデックス
-     * @param width 横方向の入力解像度
-     * @param height 縦方向の入力解像度
-     * @since 2011/11/17
+     * Builderクラスからパラメータを受け取りインスタンスを生成する
+     * @param param Builderクラスのインスタンス
+     * @since 2011/11/21
      */
-    public CvMain(int camera, int width, int height) {
+    private CvMain(Builder param) {
         // カメラ設定
-        capture = cvCreateCameraCapture(camera);
-        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, width);
-        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, height);
+        capture = cvCreateCameraCapture(param.camera);
+        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, param.width);
+        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, param.height);
+        src.setVisible(param.debug);
         
         while (true) {
             AnalyticProcess thread = new AnalyticProcess(_captureFrame(), true);
@@ -71,6 +114,6 @@ public class CvMain {
     }
     
     public static void main(String[] args) {
-        new CvMain(0);
+        new CvMain.Builder(0).debug(true).build();
     }
 }
