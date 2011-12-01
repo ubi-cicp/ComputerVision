40dea6e06300520f70ec4fab0be51262150e0dfb
Atsushi OHNO <atsushi-o@is.naist.jp>
2011-12-01 17:55:09 +0900

    ControllerからCanvasViewの切り替えを可能に

--- src/org/ubilab/cicp2011/cv/CvControllerDelegate.java
+++ src/org/ubilab/cicp2011/cv/CvControllerDelegate.java
@@ -12,6 +12,13 @@ public interface CvControllerDelegate {
      */
     public void capture();
     /**
+     * ウインドウの表示・非表示を切り替える
+     * @param key 切り替え対象のウインドウ名
+     * @param b 表示・非表示
+     * @since 2011/12/01
+     */
+    public void setVisible(String key, boolean b);
+    /**
      * プログラムを終了する
      * @since 2011/11/30
      */
